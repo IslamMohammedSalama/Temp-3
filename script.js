54855d952be089cd109a1220bbb7bb17a784a332
IslamMohamedSalama <mstkhdmjwjl17@gmail.com>
2025-03-03 10:50:10 +0200

    fix: remove unnecessary console logs from mega-menu click handling

--- script.js
+++ script.js
@@ -99,8 +99,6 @@ document.querySelector("header .main-nav>li:last-child>a").onclick = function (
 };
 
 addEventListener("click", function (event) {
-	console.log(event.target.classList);
-	console.log(event.target.matches("header .main-nav>li:last-child>a"))
 	if (
 		!event.target.matches("header .main-nav>li:last-child>a") &&
 		!event.target.matches("header .main-nav>li:last-child>.mega-menu *:not(a)") &&
